f69aa76ed11c642f646eeb62a584ae40c4fce9e4
JoseObreque <jose.obreque@mercadolibre.cl>
2023-04-10 14:09:16 -0400

    feat: add comments for the service and router layers

--- cmd/server/router/router.go
+++ cmd/server/router/router.go
@@ -7,22 +7,43 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/*
+The Router struct represents the router of the Gin application.
+
+	Fields:
+	- engine (*gin.Engine): A gin.Engine instance (for example: gin.Default).
+	- data ([]domain.Ticket): A slice of domain.Ticket instances.
+*/
 type Router struct {
 	engine *gin.Engine
 	data   []domain.Ticket
 }
 
-func NewRouter(engine *gin.Engine, data []domain.Ticket) *Router {
-	return &Router{
+/*
+The NewRouter function is a constructor of the Router struct.
+
+	Parameters:
+	 - engine (*gin.Engine): A gin.Engine instance (for example: gin.Default)
+	 - data ([]domain.Ticket): A slice of domain.Ticket objects.
+*/
+func NewRouter(engine *gin.Engine, data []domain.Ticket) Router {
+	return Router{
 		engine: engine,
 		data:   data,
 	}
 }
 
+/*
+The MapRoutes method defines all the routes associated with domain.Ticket objects. It includes:
+
+  - A route for getting the amount of tickets that have a certain destination.
+  - A route for getting the average amount of tickets that have a certain destination.
+*/
 func (r *Router) MapRoutes() {
 	repo := tickets.NewRepository(r.data)
 	service := tickets.NewService(repo)
 	handlers := handler.NewService(service)
+
 	r.engine.GET("/ticket/getByCountry/:dest", handlers.GetTicketsByCountry())
 	r.engine.GET("/ticket/getAverage/:dest", handlers.AverageDestination())
 }
--- internal/tickets/service.go
+++ internal/tickets/service.go
@@ -4,21 +4,50 @@ import (
 	"context"
 )
 
+// The Service interface acts as a service layer for managing tickets.
 type Service interface {
 	GetTotalTickets(ctx context.Context, destination string) (int, error)
 	AverageDestination(ctx context.Context, destination string) (float64, error)
 }
 
+/*
+The ServiceImpl struct is the implementation of the Service interface. It has methods
+for manage tickets data.
+
+	Fields:
+	 - repository (Repository): An instance of the Repository interface.
+*/
 type ServiceImpl struct {
 	repository Repository
 }
 
+/*
+The NewService function returns a new instance of the ServiceImpl.
+
+	Parameters:
+	 - repository (Repository): An instance of the Repository interface.
+
+	Returns:
+	 - (Service): A new instance of the Service interface.
+*/
 func NewService(repository Repository) Service {
 	return &ServiceImpl{
 		repository: repository,
 	}
 }
 
+/*
+The GetTotalTickets method returns the total number of available tickets in the "database" with a
+certain destination country in it.
+
+	Parameters:
+	 - ctx (context.Context): The context of the request.
+	 - destination (string): The country of destination.
+
+	Returns:
+	 - int: The total number of tickets with the specific destination.
+	 - error: An error if there is no tickets available in the "database".
+*/
 func (s *ServiceImpl) GetTotalTickets(ctx context.Context, destination string) (int, error) {
 	ticketsByDestination, err := s.repository.GetTicketByDestination(ctx, destination)
 	if err != nil {
@@ -27,6 +56,18 @@ func (s *ServiceImpl) GetTotalTickets(ctx context.Context, destination string) (
 	return len(ticketsByDestination), nil
 }
 
+/*
+The AverageDestination method returns the average number of tickets available in the "database" with
+a certain destination country in it.
+
+	Parameters:
+	- ctx (context.Context): The context of the request.
+	- destination (string): The country of destination.
+
+	Returns:
+	- float64: The average number of tickets available in the "database" with a certain destination country.
+	- error: An error if there is no tickets available in the "database" or no tickets found with the given destination country.
+*/
 func (s *ServiceImpl) AverageDestination(ctx context.Context, destination string) (float64, error) {
 	allTickets, err := s.repository.GetAll(ctx)
 	if err != nil {
